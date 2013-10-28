1106f0758be0b31313cc015e6b15cdc2a8bf42a6
Simon Richardson <stickupkid@gmail.com>
2013-10-28 11:26:46 +0000

    Adding Validations and States.

--- fantasy-world.js
+++ fantasy-world.js
@@ -4,6 +4,8 @@ var prelude = require('./src/prelude'),
 	combinators = require('./node_modules/fantasy-combinators/combinators'),
 	identities = require('./node_modules/fantasy-identities/identity'),
 	promises = require('./node_modules/fantasy-promises/index'),
+	validations = require('./node_modules/fantasy-validations/validation'),
+	states = require('./node_modules/fantasy-states/state'),
 	fantasy = environment();
 
 fantasy = fantasy
@@ -37,6 +39,18 @@ fantasy = fantasy
 	//  ## Fantasy-Land Promises
 	//  [https://github.com/puffnfresh/fantasy-promises](fantasy-promises)
 	//
-	.property('Promise', promises);
+	.property('Promise', promises)
+
+	//
+	//  ## Fantasy-Land Validations
+	//  [https://github.com/puffnfresh/fantasy-validations](fantasy-validations)
+	//
+	.property('Validation', validations)
+
+	//
+	//  ## Fantasy-Land States
+	//  [https://github.com/puffnfresh/fantasy-states](fantasy-states)
+	//
+	.property('State', states);
 
 exports = module.exports = fantasy;
