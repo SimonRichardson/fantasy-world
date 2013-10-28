eeb79980b308bb33c40e38e1047a773d3b35bfb5
Simon Richardson <stickupkid@gmail.com>
2013-10-28 11:05:00 +0000

    Adding daggy.

--- fantasy-world.js
+++ fantasy-world.js
@@ -1,9 +1,21 @@
 var prelude = require('./src/prelude'),
 	environment = require('./src/environment'),
+	daggy = require('./node_modules/daggy/daggy'),
 	combinators = require('./node_modules/fantasy-combinators/combinators'),
 	fantasy = environment();
 
 fantasy = fantasy
+	//
+	//  ## Daggy
+	//  [https://github.com/puffnfresh/daggy](daggy)
+	//  
+	.property('tagged', daggy.tagged)
+	.property('taggedSum', daggy.taggedSum)
+	
+	//
+	//  ## Fantasy-Land Combinators
+	//  [https://github.com/puffnfresh/fantasy-combinators](fantasy-combinators)
+	//  
 	.property('apply', combinators.apply)
 	.property('compose', combinators.compose)
 	.property('constant', combinators.constant)
